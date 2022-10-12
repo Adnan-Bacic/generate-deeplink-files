f9aefd0e23871b6d92775b954d6752ad33af97a4
Adnan Bacic <adnan-bacic@hotmail.com>
2022-10-12 14:13:48 +0200

    setup eslint + airbnb and airbnb typescript.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-base',
+        'airbnb-typescript/base'
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        project: './tsconfig.eslint.json'
+    },
+    'plugins': [
+    ],
+    'rules': {
+        
+    }
+};
